8a388f749a62b27e5ccfeda75b4b0dabfd35a234
Alan O <masteralan2001@gmail.com>
2021-04-28 19:01:37 -0400

    Work on feedback form, readme

--- readme.md
+++ readme.md
@@ -1,4 +1,18 @@
-Website for CGT 141.
+Final Project for CGT141 (Web Design) at Purdue University. We decided a game would be fun to make!
 
-# Testing
-Simply open [index.html](src/client/index.html) in your browser.
+This game was developed by Alan O'Cull, Patrick Ryan, and Shelby Hockaday.
+As of 4/28/2021, it is available to play at [flightofphoenixga.me](http://flightofphoenixga.me), though may not be online forever.
+
+# Play
+To play, simply open [index.html](www/html/index.html) in your web browser browser.
+
+# Modding
+If you would like to mod the game or add more levels, the [scripts](www/html/scripts) contains a few scripts and subfolders.
+
+Modify [engine-level](www/html/scripts/engine) scripts for fundamental changes.
+Special game-specific or template objects can be stored in [special](www/html/scripts/special) scripts for easy organization.
+Object, engine, and level tests can be stored in the [tests](www/html/scripts/tests) folder.
+Finally, if you'd like to make your own levels, you can edit [levels.js](www/html/scripts/levels.js) or create your own script that is of similar format.
+
+Remember to update your dependencies in [game.html](www/html/game.html)!
+They should also be loaded in order of use (i.e. the Vector object is used in Ray objects, so the Vector module is loaded first).
--- www/html/scripts/feedback.js
+++ www/html/scripts/feedback.js
@@ -1,24 +1,121 @@
+/**
+ * @function checkRadio
+ * @summary Checks a list of radio inputs and returns the selected one's value
+ * @param {String[]} radioNameList Document IDs of radio buttons
+ * @returns {String} Selected radio's value, or undefined if none have been checked
+ */
+function checkRadio(radioNameList) {
+    let selected = undefined;
+
+    radioNameList.forEach((value, index, arr) => {
+        const radio = document.getElementById(value);
+        if (radio && radio.checked) {
+            selected = radio.value;
+        }
+    });
+
+    return selected;
+}
+
+
 window.onload = function() {
     /** @type {HTMLForm} */
     const form = document.getElementById('feedback_form');
     const thanks = document.getElementById('thanks');
 
+    /** @type {HTMLInputElement} */
+    const overall = document.getElementById('overall_score');
+    const overallLabel = document.getElementById('overall_score_value');
+    const recommend = document.getElementById('recommend_score');
+    const recommendLabel = document.getElementById('recommend_score_value');
+    const controlsLabel = document.getElementById('controls_label');
+    const radio_controls = [
+        "controls_good",
+        "controls_okay",
+        "controls_bad",
+        "controls_unsure",
+    ];
+    const discoveryLabel = document.getElementById('discovery_label');
+    const radio_discovery = [
+        "discovery_friend",
+        "discovery_git",
+        "discovery_web",
+        "discovery_other",
+    ];
+    const featureLabel = document.getElementById('feature_label');
+    const radio_feature = [
+        "feature_levels",
+        "feature_movement",
+        "feature_obstacles",
+        "feature_art",
+        "feature_unsure",
+    ];
+    const comments = document.getElementById('comments');
+
+    overall.oninput = function () {
+        overallLabel.textContent = overall.value;
+    }
+    recommend.oninput = function () {
+        recommendLabel.textContent = recommend.value;
+    }
+    comments.onfocus = function() {
+        if (comments.textContent == 'Type here.') {
+            comments.value = '';
+            comments.textContent = '';
+        }
+
+        comments.value = comments.value.trim();
+    };
+
+
+    // Run setup
+    overall.oninput();
+    recommend.oninput();
+
+
     function validateForm(event) {
         event.preventDefault();
 
-        // Hide form so user does not submit multiple
-        form.hidden = true;
+        let verified = true;
 
+        controlsLabel.classList = '';
+        discoveryLabel.classList = '';
+        featureLabel.classList = '';
 
         // Gather and process form data
-        // console.log(FormData(form));
+        comments.onfocus(); // Clear out default text if there, and trim
 
+        const controlsScore = checkRadio(radio_controls);
+        const discoveryScore = checkRadio(radio_discovery);
+        const featureScore = checkRadio(radio_feature);
+        if (controlsScore == undefined) {
+            controlsLabel.classList = "error";
+            verified = false;
+        }
+        if (discoveryScore == undefined) {
+            discoveryLabel.classList = "error";
+            verified = false;
+        }
+        if (featureScore == undefined) {
+            featureLabel.classList = "error";
+            verified = false;
+        }
 
-        // Submit using an XMLHttpRequest()
+        if (!verified) return; // Allow form to be edited if not verified
+        form.hidden = true; // Hide form so user does not submit multiple
+        thanks.hidden = false; // Show thanks
 
+        // Submit form data using an XMLHttpRequest()
+        const data = JSON.stringify({
+            overall: overall.value,
+            recommend: recommend.value,
+            controls: controlsScore,
+            discovery: discoveryScore,
+            feature: featureScore,
+            comment: comments.value,
+        });
 
-        // Show thanks
-        thanks.hidden = false;
+        console.log("Feedback accepted! Form data:\n", data);
     }
 
     // Hide form once submitted
